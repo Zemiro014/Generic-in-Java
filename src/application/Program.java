b4c8ef76e6e3b86a3f2478de11d36f35ed919859
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-18 23:14:24 -0300

    Tipo curinga (Super tipo de List)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,27 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import entity.Product;
-import service.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		List<Product> list = new ArrayList<>();
+		
+		// O "Object" não é superTipo de "List"
+		List<Object> myObject = new ArrayList<>();
+		List <Integer> myInteger = new ArrayList<>();
+		
+		myObject = myInteger; // Erro de compilação
+		
+		
+		// O "superTipo" de List é o "?"
+		List<?> myObject1 = new ArrayList<>();
+		List <Integer> myInteger1 = new ArrayList<>();
+		
+		myObject1 = myInteger1;
 
-		String path = "c:\\temp\\in.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) 
-		{
-
-			String line = br.readLine();
-			while (line != null) 
-			{
-				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
-			}
-			
-			Product x = CalculationService.max(list);
-			System.out.println("Max: ");
-			System.out.println(x);
-
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
 	}
 
 }
