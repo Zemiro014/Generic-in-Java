6a526f26a8c4486c9b45511c022cc95345d87f8f
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-19 23:02:40 -0300

    Passando uma Lista específica para uma Lista mais genérica

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,35 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import entity.Circle;
-import entity.Rectangle;
-import entity.Shape;
-
-// Usando curinga delimitado
+// Passando uma Lista específica para uma Lista mais genérica
 public class Program {
-	public static void main(String[] args) 
-	{
-		// Covariância: Permite obter elementos de uma lista mas não permite adicionar elemento a ele.
-		List<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(5);
+
+	public static void main(String[] args) {
 		
-		List<? extends Number> list = intList;
-		Number x = list.get(0);
-		list.add(20); // Erro de compilação
-		
-		// Contravariância: Permite adicionar elementos de uma lista mas não permite obter os elementos dela.
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
 		
-		List<? super Number> myNums = myObjs;
-		myNums.add(10);
-		myNums.add(3.14);
-		Number x = myNums.get(0); // Erro de compilação
+		copy(myInts, myObjs);
+		printList(myObjs);
+		
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 	}
-	
+
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
+	}
 }
