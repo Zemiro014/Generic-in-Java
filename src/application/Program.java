dd8f61b7726dd766fd1a9734aa8147eb6d80443d
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-22 11:25:33 -0300

    Set<T> continuação: União, Interseção e Diferença

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,39 +1,34 @@
 package application;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		// Trabalhando com conjuntos: União, intercessão e diferença
 		
-		/*
-		 * Set<T> é uma interface genérica para estrutura de dados. Ela possui 3 implementações: 
- * 																						HashSet(mais rápido "O(1)" mas não ordena os elementos);
- * 																						TreeSet(menos rápido que o HashSet, mas ordena os elementos);
- * 																						LinkedHashSet (menos rápido retorno os elementos do jeito que foram colocado)
- * 
- *  Set<T> é uma estrutura que armazena elementos do tipo T sem posições.  Não permite buscar elementos por índices
-		 */ 
-		Set<String> set = new LinkedHashSet<>();
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 		
-		set.add("Tv");
-		set.add("Tablet");
-		set.add("Notebook");
-		set.add("Tik Tok");
-		set.add("Ta");
-		set.add("Pa");
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		//union: adicionar ao conjunto "c" aqueles elementos do conjunto "a" que não existem no conjunto "c"
+		c.addAll(b);
+		System.out.println(c);
 		
-		//Algumas Operações
-		set.remove("Tablet"); // Remove o elemento Tablet da estrutura de dados
-		set.removeIf(x -> x.length() >= 3); // Remove os elementos que satisfazem as condições especificados
-		set.removeIf(x -> x.charAt(0) == 'T');
+		//intersection
+		Set<Integer> d = new TreeSet<>(a);
+		//intersection: reter ao conjunto "d" só aqueles elementos do conjunto que combinam com algum elemento do conjunto "b"
+		d.retainAll(b);
+		System.out.println(d);
 		
-		System.out.println(set.contains("Notebook"));
-		
-		for (String p : set) {
-		System.out.println(p);
-		}
+		//difference
+		Set<Integer> e = new TreeSet<>(a);
+		//intersection: remover do conjunto "e" aqueles elementos do conjunto que não possuem combinação com algum elemento do conjunto "b"
+		e.removeAll(b);
+		System.out.println(e);
 	}
 }
