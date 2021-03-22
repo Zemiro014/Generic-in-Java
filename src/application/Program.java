c97d83335e65314e3db7af23bf7903879a3e27c8
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-22 11:12:20 -0300

    Trabalhando com Set<T> e suas implementações e funções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,39 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-import entity.Client;
-
-// ENtendendo o funcionamento do HashCode e Equals
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Client maria = new Client("Maria", "maria@gmail.com");
-		Client alex = new Client("Maria", "maria@gmail.com");
 		
-		System.out.println(maria.hashCode());
-		System.out.println(alex.hashCode());
+		/*
+		 * Set<T> é uma interface genérica para estrutura de dados. Ela possui 3 implementações: 
+ * 																						HashSet(mais rápido "O(1)" mas não ordena os elementos);
+ * 																						TreeSet(menos rápido que o HashSet, mas ordena os elementos);
+ * 																						LinkedHashSet (menos rápido retorno os elementos do jeito que foram colocado)
+ * 
+ *  Set<T> é uma estrutura que armazena elementos do tipo T sem posições.  Não permite buscar elementos por índices
+		 */ 
+		Set<String> set = new LinkedHashSet<>();
 		
-		System.out.println(maria.equals(alex)); // Faz a comparação do conteúdo dos 2 objectos
-		System.out.println(maria==alex); // Faz a comparação das referências dos 2 objectos
+		set.add("Tv");
+		set.add("Tablet");
+		set.add("Notebook");
+		set.add("Tik Tok");
+		set.add("Ta");
+		set.add("Pa");
+		
+		//Algumas Operações
+		set.remove("Tablet"); // Remove o elemento Tablet da estrutura de dados
+		set.removeIf(x -> x.length() >= 3); // Remove os elementos que satisfazem as condições especificados
+		set.removeIf(x -> x.charAt(0) == 'T');
+		
+		System.out.println(set.contains("Notebook"));
+		
+		for (String p : set) {
+		System.out.println(p);
+		}
 	}
 }
